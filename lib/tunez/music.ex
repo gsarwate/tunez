cc0592bba3b44b01b6740e6b3dbfcebfea31a55a
gsarwate <4589380+gsarwate@users.noreply.github.com>
2025-10-06 02:31:36 -0700

    add album actions

--- lib/tunez/music.ex
+++ lib/tunez/music.ex
@@ -10,6 +10,13 @@ defmodule Tunez.Music do
       define :destroy_artist, action: :destroy
     end
 
+    resource Tunez.Music.Album do
+      define :create_album, action: :create
+      define :get_album_by_id, action: :read, get_by: :id
+      define :update_album, action: :update
+      define :destroy_album, action: :destroy
+    end
+
     resource Tunez.Music.Album
   end
 end
